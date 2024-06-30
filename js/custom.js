98bbc4c4042e905df70ba191cc5e327dc592826b
dhyey-R12 <rajparadhyey12@gmail.com>
2024-07-01 01:16:45 +0530

    Project Init

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+gsap.registerPlugin(ScrollTrigger)
+
+document.addEventListener("DOMContentLoaded", function () {
+    const footer = document.querySelector(".footer");
+    const pinnedSections = gsap.utils.toArray(".card");
+    // const lastCard = document.querySelector(".card.scroll");
+    const lastCard = pinnedSections.pop();
+    
+
+    pinnedSections.forEach((section, index, sections) => {
+
+        let img = section.querySelector(".img");
+
+
+        let nextSection = sections[index + 1] || lastCard;
+        let endSectionPoint = `top+=${nextSection.offsetTop - section.offsetTop} top`;
+        gsap.to(section, {
+            scrollTrigger: {
+                trigger: section,
+                start: "top top",
+                end: index === sections.length ? `+=S{lastCard.offsetHeight / 2}` : footer.offsetTop - window.innerHeight,
+                pin: true,
+                pinSpacing: false,
+                scrub: 1,
+                // markers: true,
+            }
+        });
+
+        gsap.fromTo(img,
+            {
+                scale: 1
+            },
+            {
+                scale: 0.5,
+                ease: "none",
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top top",
+                    end: endSectionPoint,
+                    scrub: 1,
+                    // markers: true,
+                }
+            }
+        )
+    });
+
+    const heroH1 = document.querySelector(".hero h1");
+    ScrollTrigger.create({
+        trigger: document.body,
+        start: "top top",
+        // end: "+=400vh",
+        end: window.outerHeight / 2,
+        scrub: 1,
+        // markers: true,
+        onUpdate: (self) => {
+            let opacityProgress = self.progress;
+            heroH1.style.opacity = 1 - opacityProgress;
+
+        }
+    });
+});
+
